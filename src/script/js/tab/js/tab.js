83b592a861f36f43e197a23c4d7c78adc8fed0a1
geek_yoshida <kyoshida@geek.co.jp>
2020-04-09 11:30:06 +0900

    [update] ・[WIP] 【タブメニュー】クリックで表示させる記述2

--- src/script/js/tab/js/tab.js
+++ src/script/js/tab/js/tab.js
@@ -1,9 +1,15 @@
 import '../../../_modules/module';
 
-const tabcontent1 = document.querySelector('.tab_contentItem1');
-
-// classを取得して、クリックイベントとして関数を追加
-
+// 対象のclassを取得して、クリックイベントの関数を追加
+//   タブ中身を、toggleメソッドでactiveクラスを付け外しすることで、表示/非表示を切り替える。
 document.querySelector('.tab_navItem1').onclick = () => {
-  tabcontent1.style.display = 'block';
+  document.querySelector('.tab_contentItem1').classList.toggle('is-active');
 };
+// document.addEventListener('DOMcontentLoaded', () => {
+//   const tab_navItem = document.querySelector('.tab_navItem');
+//   const tab_contentItem = document.querySelector('.tab_contentItem');
+
+//   tab_navItem.onclick = () => {
+//     tab_contentItem.classList.toggle('is-active');
+//   };
+// });
