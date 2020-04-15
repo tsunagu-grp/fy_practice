4fa5fa98bf3d61108c5446f2aad8f46582498abb
geek_yoshida <kyoshida@geek.co.jp>
2020-04-15 14:06:19 +0900

    [update] ・【タブメニュー】クリックで表示させる：完成

--- src/script/js/tab/js/tab.js
+++ src/script/js/tab/js/tab.js
@@ -1,15 +1,30 @@
 import '../../../_modules/module';
 
-// 対象のclassを取得して、クリックイベントの関数を追加
-//   タブ中身を、toggleメソッドでactiveクラスを付け外しすることで、表示/非表示を切り替える。
-document.querySelector('.tab_navItem1').onclick = () => {
-  document.querySelector('.tab_contentItem1').classList.toggle('is-active');
-};
-// document.addEventListener('DOMcontentLoaded', () => {
-//   const tab_navItem = document.querySelector('.tab_navItem');
-//   const tab_contentItem = document.querySelector('.tab_contentItem');
-
-//   tab_navItem.onclick = () => {
-//     tab_contentItem.classList.toggle('is-active');
-//   };
-// });
+// 読み込みでイベント発火する関数
+document.addEventListener('DOMContentLoaded', () => {
+  // tab_navItemのHTMLCollectionを取得して、変数tabsに格納
+  const tabs = document.getElementsByClassName('tab_navItem');
+  // .tan_navItemの要素に対し、要素の数だけclickイベントの関数tabSwitchを繰り返し
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', tabSwitch);
+  }
+  // タブをクリックすると実行する関数
+  function tabSwitch() {
+    // タブのclassの値を変更する記述：.is-activeを持つ要素から.is-activeを削除、そして追加。
+    document
+      .getElementsByClassName('is-active')[0]
+      .classList.remove('is-active');
+    this.classList.add('is-active');
+    // タブコンテンツのclassの値を変更する記述：
+    // .is-showを持つ要素から.is-showを削除、そして追加。
+    // slice.call()メソッドにより、tabsの要素を配列にする。
+    // その配列の中からクリックしたタブのインデックス番号を返し、
+    // 同じインデックス番号を持つ.tab_contentItemの要素に.is-showを与える。
+    document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+    const arrayTabs = Array.prototype.slice.call(tabs);
+    const index = arrayTabs.indexOf(this);
+    document
+      .getElementsByClassName('tab_contentItem')
+      [index].classList.add('is-show');
+  }
+});
