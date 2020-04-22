021063edaeed2ba96575acfd03a8a1c2d6e14cf4
geek_yoshida <kyoshida@geek.co.jp>
2020-04-22 15:33:47 +0900

    [update] ・[WIP]【アコーディオンメニュー】css調整途中

--- src/script/js/accordion/js/accordion.js
+++ src/script/js/accordion/js/accordion.js
@@ -1,1 +1,14 @@
 import '../../../_modules/module';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // アコーディオンメニュー＆アコーディオンコンテント要素を取得し変数に格納
+  const nav = document.getElementsByClassName('accordion_navItem');
+  const content = document.getElementsByClassName('accordion_contentItem');
+  // 表示非表示のclassを付け外しする関数を定義
+  function showContent() {
+    content[0].classList.toggle('show');
+    content[0].classList.toggle('hide');
+  }
+  // タブメニューがクリックされたら実行
+  nav[0].addEventListener('click', showContent);
+});
