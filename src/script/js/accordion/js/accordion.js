35d303e657005e6e9dfc1bc41efe287eb0675cb2
geek_yoshida <kyoshida@geek.co.jp>
2020-05-14 20:04:05 +0900

    [update] ・[WIP] アコーディオン 1つ目のみ動作

--- src/script/js/accordion/js/accordion.js
+++ src/script/js/accordion/js/accordion.js
@@ -2,13 +2,21 @@ import '../../../_modules/module';
 
 document.addEventListener('DOMContentLoaded', () => {
   // アコーディオンメニュー＆アコーディオンコンテント要素を取得し変数に格納
-  const nav = document.getElementsByClassName('accordion_navItem');
-  const content = document.getElementsByClassName('accordion_contentItem');
-  // 表示非表示のclassを付け外しする関数を定義
-  function showContent() {
-    content[0].classList.toggle('show');
-    content[0].classList.toggle('hide');
-  }
-  // タブメニューがクリックされたら実行
+  const nav = document.querySelectorAll('h2.accordion_navItem');
+  const content = document.querySelectorAll('p.accordion_contentItem');
+  // 非表示・表示させる関数
+  const showContent = () => {
+    // 非表示のclassがあれば、非表示classと、2回目以降に閉じるclassも消す。
+    if (content[0].classList.contains('hidden')) {
+      content[0].classList.remove('hidden', 'slideout');
+      // 非表示classがなければ、閉じるclassを追加し、遅らせて非表示classを追加。
+    } else {
+      content[0].classList.add('slideout');
+      // 非表示のclassの付与を200ms遅らせる。（cssアニメーション時間と合わせる）
+      setTimeout(() => {
+        content[0].classList.add('hidden');
+      }, 200);
+    }
+  };
   nav[0].addEventListener('click', showContent);
 });
--- src/script/js/tab/js/tab.js
+++ src/script/js/tab/js/tab.js
@@ -4,27 +4,28 @@ import '../../../_modules/module';
 document.addEventListener('DOMContentLoaded', () => {
   // tab_navItemのHTMLCollectionを取得して、変数tabsに格納
   const tabs = document.getElementsByClassName('tab_navItem');
-  // .tan_navItemの要素に対し、要素の数だけclickイベントの関数tabSwitchを繰り返し
-  for (let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', tabSwitch);
+  if (tabs) {
+    // .tan_navItemの要素に対し、要素の数だけclickイベントの関数tabSwitchを繰り返し。
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].addEventListener('click', tabSwitch);
+    }
   }
   // タブをクリックすると実行する関数
   function tabSwitch() {
+    const activeTab = document.getElementsByClassName('is-active');
+    const showContent = document.getElementsByClassName('is-show');
+    const contentItem = document.getElementsByClassName('tab_contentItem');
     // タブのclassの値を変更する記述：.is-activeを持つ要素から.is-activeを削除、そして追加。
-    document
-      .getElementsByClassName('is-active')[0]
-      .classList.remove('is-active');
+    activeTab[0].classList.remove('is-active');
     this.classList.add('is-active');
     // タブコンテンツのclassの値を変更する記述：
     // .is-showを持つ要素から.is-showを削除、そして追加。
-    // slice.call()メソッドにより、tabsの要素を配列にする。
-    // その配列の中からクリックしたタブのインデックス番号を返し、
-    // 同じインデックス番号を持つ.tab_contentItemの要素に.is-showを与える。
-    document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+    // slice.call()により、tabsの要素を配列にする。
+    // その配列の中から、クリックされたタブのインデックス番号(is-activeを持つ要素の番号)を返し、
+    // 同じインデックス番号を持つ.tab_contentItemに.is-showを与える。
+    showContent[0].classList.remove('is-show');
     const arrayTabs = Array.prototype.slice.call(tabs);
     const index = arrayTabs.indexOf(this);
-    document
-      .getElementsByClassName('tab_contentItem')
-      [index].classList.add('is-show');
+    contentItem[index].classList.add('is-show');
   }
 });
