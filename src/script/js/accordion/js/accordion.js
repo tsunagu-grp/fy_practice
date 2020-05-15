ab9602215a416d645d8684ad6dc053d8a4d1f086
geek_yoshida <kyoshida@geek.co.jp>
2020-05-15 11:43:47 +0900

    [update] ・【アコーディオンメニュー】JS style属性で要素を動かすように変更

--- src/script/js/accordion/js/accordion.js
+++ src/script/js/accordion/js/accordion.js
@@ -1,22 +1,17 @@
 import '../../../_modules/module';
 
-document.addEventListener('DOMContentLoaded', () => {
-  // アコーディオンメニュー＆アコーディオンコンテント要素を取得し変数に格納
-  const nav = document.querySelectorAll('h2.accordion_navItem');
-  const content = document.querySelectorAll('p.accordion_contentItem');
-  // 非表示・表示させる関数
-  const showContent = () => {
-    // 非表示のclassがあれば、非表示classと、2回目以降に閉じるclassも消す。
-    if (content[0].classList.contains('hidden')) {
-      content[0].classList.remove('hidden', 'slideout');
-      // 非表示classがなければ、閉じるclassを追加し、遅らせて非表示classを追加。
+const nav = document.querySelectorAll('h2.accordion_nav');
+console.log(nav);
+for (let i = 0; i < nav.length; i++) {
+  nav[i].addEventListener('click', function() {
+    this.classList.toggle('active');
+    // navの次にある要素を取得。
+    const content = this.nextElementSibling;
+    // contentのstyle属性のheightがあるか判定、あればnull、無ければcontentの要素の高さを属性として入れる。
+    if (content.style.height) {
+      content.style.height = null;
     } else {
-      content[0].classList.add('slideout');
-      // 非表示のclassの付与を200ms遅らせる。（cssアニメーション時間と合わせる）
-      setTimeout(() => {
-        content[0].classList.add('hidden');
-      }, 200);
+      content.style.height = content.scrollHeight + 'px';
     }
-  };
-  nav[0].addEventListener('click', showContent);
-});
+  });
+}
